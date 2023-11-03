bc2f118fdbd8b73766445325b62af8c173ca08be
Eduardo <bestessays001@gmail.com>
2023-11-03 15:32:58 +0300

    new pc test

--- testapp/settings.py
+++ testapp/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['kpsea.testprepken.com','www.kpsea.testprepken.com']
-# ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
 
 # Application definition
